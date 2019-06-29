9e6ff2405a1c4e803330fea8d64cebc14506f285
Danielle Lewandowski <dani@webdevdani.com>
2019-06-29 16:36:41 -0400

    Working on writing methods to store lists in firebase database

--- src/modules/Firebase/Firebase.js
+++ src/modules/Firebase/Firebase.js
@@ -1,5 +1,6 @@
 import app from 'firebase/app';
 import 'firebase/auth';
+import 'firebase/database';
 
 const config = {
   apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
@@ -10,11 +11,17 @@ const config = {
   messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
 };
 
+// Database Keys
+const USERS = 'users';
+const LISTS = 'lists';
+const LIST_ITEMS = 'listItems';
+
 class Firebase {
     constructor() {
         app.initializeApp(config);
 
         this.auth = app.auth();
+        this.db = app.database();
     }
 
     /* *** Auth API *** */
@@ -34,6 +41,66 @@ class Firebase {
     doPasswordUpdate = password => (
         this.auth.currentUser.updatePassword(password)
     );
+
+    getAuthedUserId = () => this.auth.currentUser ? this.auth.currentUser.id : null;
+
+
+    /* *** Writing data *** */
+
+    createNewList = () => {
+        const userId = this.getAuthedUserId();
+        const newListItemsId = this.db.ref(LIST_ITEMS).push().key;
+
+        const newListId = this.db.ref(LISTS).push({
+            [LIST_ITEMS]: newListItemsId,
+            users: {
+                [userId]: 'creator',
+            },
+        }).key;
+
+        const updates = {};
+
+        // Set the list on the list items set
+        updates[`/${LIST_ITEMS}/${newListItemsId}/list`] = newListId;
+
+        // Associate the list to the user
+        updates[`/${USERS}/${userId}/${LISTS}/${newListId}`] = 'creator';
+
+        this.db.ref().update(updates);
+    };
+
+    saveList = (listId, data) => {
+        // save list
+        // & save list items
+    };
+
+    deleteList = (listId) => {
+        // remove list from user
+        // remove list
+        // remove listItem
+    };
+
+    updateListItems = (listItemsId, data) => {
+        // save list items
+    };
+
+
+    /* *** Reading data *** */
+
+    getList = (listId) => {
+        // get list
+        // get list items
+        // get user?
+    };
+
+    getUsersLists = (userId) => {
+        // get users/lists
+        // then get lists for each id
+    };
+
+    getPublicLists = () => {
+        // get lists, paginated
+    }
 }
 
 // When ready to deploy, create prod version of firebase project
