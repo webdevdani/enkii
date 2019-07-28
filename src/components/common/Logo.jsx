5739c753ac2728057d1feb938564bf5f87e868fd
Danielle Lewandowski <dani@webdevdani.com>
2019-07-28 18:59:49 -0400

    Add logo and favicon to site

--- src/components/common/Logo.jsx
+++ src/components/common/Logo.jsx
@@ -2,11 +2,12 @@ import React from 'react';
 import { NavLink } from 'react-router-dom';
 
 import { HOME } from 'constants/routes';
+import EnkiiLogo from 'assets/EnkiiLogo';
 
 const Logo = (props) => {
     return (
         <NavLink exact to={HOME}>
-            enkii
+            <EnkiiLogo />
         </NavLink>
     );
 }
